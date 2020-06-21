3ab088b18ce9b1c82e328e62506c45b38ad2407f
Felix <jonfelixrico@gmail.com>
2020-06-21 13:51:36 +0800

    chore(#3): Integrate ConfigUtils in the Nest.js bootstrapper

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import ConfigUtils from './utils/config.util'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(ConfigUtils.HTTP_PORT)
 }
 bootstrap()
