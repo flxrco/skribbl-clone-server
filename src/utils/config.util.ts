1255b2288f828444682c7cebf0c8d6b3e03f6fa8
Felix <jonfelixrico@gmail.com>
2020-06-21 13:46:55 +0800

    feat(#3): Create utils to handle environment variables

---
+++ src/utils/config.util.ts
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const { HTTP_PORT, WS_PORT } = process.env
+
+export default class ConfigUtils {
+  static get HTTP_PORT(): number {
+    return HTTP_PORT ? parseInt(HTTP_PORT) : 3000
+  }
+
+  static get WS_PORT(): number {
+    return WS_PORT ? parseInt(WS_PORT) : 3001
+  }
+}
