dd0f911ebf43472bc4293c929eba5df25c36c578
Felix <jonfelixrico@gmail.com>
2020-06-21 13:51:55 +0800

    feat(#3): Create the module for websocket gateways

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+import { WsGatewaysModule } from './ws-gateways/ws-gateways.module';
 
 @Module({
-  imports: [],
+  imports: [WsGatewaysModule],
   controllers: [],
   providers: [],
 })
